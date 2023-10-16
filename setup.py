4900b98c37a99067f0a634763fae69612ae196b5
guozh <guozhengyang980525@yahoo.co.jp>
2023-10-16 17:08:39 +0800

    initial commitment

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-import setuptools
-install = ['numpy >= 1.17.2','opencv-python >= 4.7.0.12','scipy == 1.11.2','tqdm == 4.66.1','biopython == 1.81']
-setup = ['numpy','scipy','opencv-python','tqdm','pickle','biopython']
+from setuptools import find_packages
+from distutils.core import setup
+
+install = ['numpy >= 1.17.2', 'opencv-python >= 4.7.0.12', 'scipy == 1.11.2', 'tqdm == 4.66.1', 'biopython == 1.81']
 
 
-import setuptools
-setuptools.setup(
-    name='dia',
-    version='1.0',
-    description='this is a program for dia',
-    author='Guo Zhengyang',
+with open("Readme.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='adams',
+    version='0.0.6',
+    description='this is a program for fast protein structure search',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Guo_Zhengyang',
     author_email='guozy23@mails.tsinghua.edu.cn',
-	setup_requires=setup,
-	install_requires=install,
-    packages=setuptools.find_packages(),
-   )
+    install_requires=install,
+    packages=find_packages(),
+    license='GNU-GPL 3.0',
+    classifiers=['Programming Language :: Python :: 3', 'Development Status :: 4 - Beta', ],
+    url='https://github.com/young55775/ADAMS-developing',
+    include_package_data=True
+)
